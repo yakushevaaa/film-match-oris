8c98df78c2b892e2b4ccd45b2b06082cdd16c99c
yakushevaaa <muzisnvlada@gmail.com>
2025-06-12 20:30:43 +0300

    Add god

--- backend/SocialNetwork/Controllers/UserController.cs
+++ backend/SocialNetwork/Controllers/UserController.cs
@@ -2,17 +2,30 @@ using FilmMatch.Application.Features.Users.Commands.RegisterUser;
 using FilmMatch.Application.Features.Users.Queries.LoginUser;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using FilmMatch.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Identity;
 
 namespace FilmMatch.Controllers ;
 
     [ApiController]
     [Route("[controller]")]
-    public class UserController(IMediator mediator) : ControllerBase
+    public class UserController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        private readonly IUserService _userService;
+        private readonly UserManager<IdentityUser<Guid>> _userManager;
+
+        public UserController(IMediator mediator, IUserService userService, UserManager<IdentityUser<Guid>> userManager)
+        {
+            _mediator = mediator;
+            _userService = userService;
+            _userManager = userManager;
+        }
+
         [HttpPost("register")]
         public async Task<IActionResult> RegisterUser([FromBody] RegisterUserCommand request)
         {
-            return Ok(await mediator.Send(new RegisterUserCommand()
+            return Ok(await _mediator.Send(new RegisterUserCommand()
             {
                 Password = request.Password,
                 Email = request.Email,
@@ -23,10 +36,29 @@ namespace FilmMatch.Controllers ;
         [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginUserQuery request)
         {
-            return Ok(await mediator.Send(new LoginUserQuery()
+            return Ok(await _mediator.Send(new LoginUserQuery()
             {
                 Email = request.Email,
                 Password = request.Password
             }));
+        }
+
+        public class MakeAdminRequest
+        {
+            public string Email { get; set; }
+        }
+
+        [HttpPost("make-admin")]
+        public async Task<IActionResult> MakeAdmin([FromBody] MakeAdminRequest request)
+        {
+            var currentUser = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (currentUser == null || !await _userService.IsGodAsync(currentUser))
+                return Forbid();
+
+            var result = await _userService.MakeAdminAsync(request.Email);
+            if (!result.Succeeded)
+                return BadRequest(result.Errors);
+
+            return Ok("Admin rights granted");
         }
     }
